1b828ac6864ef04b259c00f2b132c352944ed1c8
pickonefish <pickonefish@gmail.com>
2025-03-15 06:28:00 +0000

    worldgenoverride

---
+++ .klei/DoNotStarveTogether/MyDediServer/Master/worldgenoverride.lua
@@ -0,0 +1,17 @@
+KLEI     1 return {
+	override_enabled = true,
+	worldgen_preset = "SURVIVAL_TOGETHER",
+	settings_preset = "SURVIVAL_TOGETHER",
+	overrides = {
+		bats_setting = "never",
+		evergreen_regrowth = "never",
+		fishschools = "always",
+		grassgekkos = "never",
+		hounds = "rare",
+		lureplants = "never",
+		prefabswaps_start = "classic",
+		specialevent = "winters_feast",
+		twiggytrees_regrowth = "never",
+		wildfires = "never",
+	},
+}
