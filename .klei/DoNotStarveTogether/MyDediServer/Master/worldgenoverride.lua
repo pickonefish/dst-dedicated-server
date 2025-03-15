91708e646cc207b60150a0901cba016863f1173f
PickOneFish <pickonefish@gmail.com>
2025-03-15 20:50:55 +0800

    Update worldgenoverride.lua

--- .klei/DoNotStarveTogether/MyDediServer/Master/worldgenoverride.lua
+++ .klei/DoNotStarveTogether/MyDediServer/Master/worldgenoverride.lua
@@ -3,5 +3,9 @@ KLEI     1 return {
 	worldgen_preset = "RELAXED",
 	settings_preset = "RELAXED",
 	overrides = {
+	    autumn = "longseason",  -- 秋天變長
+	    winter = "shortseason", -- 冬天變短
+	    spring = "shortseason", -- 春天變短
+	    summer = "shortseason", -- 夏天變短
 	},
 }
